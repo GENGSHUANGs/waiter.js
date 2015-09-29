d8c7dd8f42d0f1e5c89d02f08492341f170af915
GENGSHUANGs <abe.keng@gmail.com>
2015-09-29 08:47:23 +0800

    将 timerout.js 合并进来

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # waiter.js
+
 等待多个异步任务完成
+------
 
 usage:
 ```javascript
@@ -10,3 +12,16 @@ var counter = new Waiter(2,function(){
 window.setTimeout(counter,1000);
 window.setTimeout(counter,2000);
 ```
+
+检测超时，以保证最低延迟时间
+------
+
+usage:
+```javascript
+var callback = new Waiter.Timerout(function(){
+        // @a do something
+},3000); 
+app.startup(callback);
+```
+如果app.startup 3s内启动完成，则最少等待3秒后才执行 @a 部分代码；
+如果app.startup 超过3s启动，则启动完成后立即执行@a 部分代码
--- waiter.js
+++ waiter.js
@@ -3,7 +3,7 @@
 
 	/**
         异步等待 */
-        root.Waiter = function(length, ready) {
+        var Waiter = function(length, ready) {
                 var count = 0;
                 return function() {
                         count++;
@@ -12,4 +12,23 @@
                         }
                 };
         };
+
+        /**
+        超时等待期 
+        用于：最短等待时间
+        usage:
+        var callback = new Waiter.Timerout(function(){
+                // @a do something
+        },3000); 
+        app.startup(callback);
+        如果app.startup 3s内启动完成，则最少等待3秒后才执行 @a 部分代码
+        如果app.startup 超过3s启动，则启动完成后立即执行@a 部分代码
+        */
+        Waiter.Timerout = function(callback, delay) {
+                var counter = new Waiter(2,callback);
+                setTimeout(counter,delay);
+                return counter;
+        };
+
+        root.Waiter = Waiter;
 })(this);
